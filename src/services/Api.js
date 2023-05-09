744422c631148be572ee85bc3fb07f4afdbc6059
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-09 17:01:57 +0300

    Create Api.js

---
+++ src/services/Api.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+
+
+
+
+// axios.defaults.baseURL ='https://6458a0d48badff578ef6314e.mockapi.io/users';
+// export const fetchUsers = async (page) => {
+//     const { data } = await axios.get(`/users?page=${page}&limit=3`)
+//     return data;
+// }
