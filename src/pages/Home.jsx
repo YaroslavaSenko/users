50feb484c81fe60b4f6e785c704e70560f383da5
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-09 17:01:01 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Container } from './Home.styled';
+
+
+const Home = () => {
+ 
+  return (
+    <Container>
+      
+      <h1>Welcome user!</h1>
+      
+    </Container>
+  );
+};
+
+export default Home;
