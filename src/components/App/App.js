79674ead9c2a385032fe7bdd334a1b552e27ee9e
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-09 16:59:26 +0300

    Create App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {  Route, Routes } from 'react-router-dom';
+import { Layout } from '../../components/Layout/Layout';
+import Home from '../../pages/Home';
+import Tweets from '../../pages/Tweets';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="tweets" element={<Tweets />} />        
+        <Route path="*" element={<Home />}/>
+          </Route>
+    </Routes>
+  );
+}
+
+export default App;
